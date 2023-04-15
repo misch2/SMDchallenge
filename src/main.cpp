d53c0ed0ff11c1c8c3afb9426667ecde3cc9c35c
Michal Schwarz <michal.schwarz@gmail.com>
2023-04-15 19:23:28 +0200

    final

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,7 @@
 #define LED_PIN4 PB3
 #define LED_PIN5 PB4
 
-// #define DELAY_CALIBRATION 10  // not 1000, because of the chip clock
-
 void setup() {
-  // put your setup code here, to run once:
-
   // set LED pins as OUTPUT
   DDRB |= (1 << LED_PIN1);
   DDRB |= (1 << LED_PIN2);
@@ -37,21 +33,87 @@ void sequential_on_off(char pin1, char pin2, char pin3, char pin4, char pin5) {
 
   // toggle LED pins off
   delay_ms = 125;
-  PORTB ^= _BV(pin5);
-  delay(delay_ms);
-  PORTB ^= _BV(pin4);
-  delay(delay_ms);
-  PORTB ^= _BV(pin3);
+  PORTB ^= _BV(pin1);
   delay(delay_ms);
   PORTB ^= _BV(pin2);
   delay(delay_ms);
-  PORTB ^= _BV(pin1);
+  PORTB ^= _BV(pin3);
+  delay(delay_ms);
+  PORTB ^= _BV(pin4);
+  delay(delay_ms);
+  PORTB ^= _BV(pin5);
   delay(delay_ms);
 }
 
+void blink() {
+  PORTB ^= _BV(LED_PIN1);
+  PORTB ^= _BV(LED_PIN2);
+  PORTB ^= _BV(LED_PIN3);
+  PORTB ^= _BV(LED_PIN4);
+  PORTB ^= _BV(LED_PIN5);
+  delay(50);
+
+  PORTB ^= _BV(LED_PIN1);
+  PORTB ^= _BV(LED_PIN2);
+  PORTB ^= _BV(LED_PIN3);
+  PORTB ^= _BV(LED_PIN4);
+  PORTB ^= _BV(LED_PIN5);
+  delay(50);
+}
+
+void from_side_to_center() {
+  int delay_ms = 125;
+  PORTB ^= _BV(LED_PIN1);
+  PORTB ^= _BV(LED_PIN5);
+  delay(delay_ms);
+  PORTB = 0;
+  PORTB ^= _BV(LED_PIN2);
+  PORTB ^= _BV(LED_PIN4);
+  delay(delay_ms);
+  PORTB = 0;
+  PORTB ^= _BV(LED_PIN3);
+  delay(delay_ms);
+  PORTB = 0;
+  delay(250);
+}
+
+void from_center_to_side() {
+  int delay_ms = 125;
+  PORTB ^= _BV(LED_PIN3);
+  delay(delay_ms);
+  PORTB = 0;
+  PORTB ^= _BV(LED_PIN2);
+  PORTB ^= _BV(LED_PIN4);
+  delay(delay_ms);
+  PORTB = 0;
+  PORTB ^= _BV(LED_PIN1);
+  PORTB ^= _BV(LED_PIN5);
+  delay(delay_ms);
+  PORTB = 0;
+  delay(250);
+}
+
 void loop() {
-
+  // from top to down
   sequential_on_off(LED_PIN1, LED_PIN2, LED_PIN3, LED_PIN4, LED_PIN5);
+  delay(500);
 
+  // from down to top
   sequential_on_off(LED_PIN5, LED_PIN4, LED_PIN3, LED_PIN2, LED_PIN1);
+  delay(500);
+
+  for (int i = 1; i <= 5; i++) {
+    from_side_to_center();
+  }
+  delay(500);
+
+  for (int i = 1; i <= 5; i++) {
+    from_center_to_side();
+  }
+  delay(500);
+
+  for (int i = 1; i <= 10; i++) {
+    blink();
+  }
+  delay(1000);
 }
